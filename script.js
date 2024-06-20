de021b3d06c654e9197f32305f04eebc89953916
MaybeDonuts <165258949+MaybeDonuts@users.noreply.github.com>
2024-06-20 13:01:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -125,4 +125,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const updateScreen = () => {
         calculatorScreen.value = previousInput + ' ' + operator + ' ' + currentInput;
     };
+    document.getElementById('exit').addEventListener('click', function() {
+    window.close();
 });
